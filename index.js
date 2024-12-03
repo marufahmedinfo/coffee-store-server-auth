bb5a64a820412d7097be4d36406b3fef2740f4b9
marufahmedinfo <marufahmedinfos@gmail.com>
2024-12-03 14:28:07 +0600

    added full

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qihkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri)
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +27,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+
+        // await client.connect();
+
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
         const usersCollection = client.db('coffeeDB').collection('users');
         // Coffee Related API
@@ -128,10 +130,13 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+
+        // await client.db("admin").command({ ping: 1 });
+        
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
+
         // await client.close();
     }
 }
